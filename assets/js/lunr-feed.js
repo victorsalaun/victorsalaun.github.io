ac29ff053aa562b135afe98a9aedc21b9a90c4d7
victorsalaun <victor.salaun@gmail.com>
2018-11-27 12:11:57 +0000

    add CodePen resources

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -21,7 +21,7 @@ var index = lunr(function () {
     index.add({
       title: "Signets Design",
       category: ["others"],
-      content: "CodePen\n\n\n  Flying Carrot\n  Low Poly Art Generator\n  Time\n\n",
+      content: "CodePen\n\n\n  Flying Carrot\n  Gravity Reverse\n  Kong3D\n  Low Poly Art Generator\n  Quarto\n  Time\n\n",
       tags: ["resources","design"],
       id: 1
     });
@@ -140,7 +140,7 @@ var store = [{
     "image": null,
     "date": "November 5, 2018",
     "category": ["others"],
-    "excerpt": "CodePen\n\n\n  Flying Carrot\n  Low Poly Art Generator\n  Time\n\n"
+    "excerpt": "CodePen\n\n\n  Flying Carrot\n  Gravity Reverse\n  Kong3D\n  Low Poly Art Generator\n  Quarto\n  Time\n\n"
 },{
     "title": "Ressources Jeux Vidéo",
     "link": "/dev/video-games-resources.html",
