30e805c3b20bb3252a34ef8ef01f1c12d8e584cf
victorsalaun <victor.salaun@gmail.com>
2018-12-31 00:20:31 +0000

    add "cloudbees / 8-cicd-best-practices-your-devops-journey" devops article

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -111,7 +111,7 @@ var index = lunr(function () {
     index.add({
       title: "Ressources DevOps",
       category: ["devops"],
-      content: "\n    \n\n\nAticles\n\n\n  octo / gardez-les-cles-de-votre-infrastructure-a-labri-avec-vault\n\n\nTools\n\n\n  awesome-scalability\n  developer-roadmap\n  hubcommander\n\n",
+      content: "\n    \n\n\nArticles\n\n\n  cloudbees / 8-cicd-best-practices-your-devops-journey\n  octo / gardez-les-cles-de-votre-infrastructure-a-labri-avec-vault\n\n\nTools\n\n\n  awesome-scalability\n  developer-roadmap\n  hubcommander\n\n",
       tags: ["resources"],
       id: 11
     });
@@ -246,7 +246,7 @@ var store = [{
     "image": null,
     "date": "September 2, 2018",
     "category": ["devops"],
-    "excerpt": "\n    \n\n\nAticles\n\n\n  octo / gardez-les-cles-de-votre-infrastructure-a-labri-avec-vault\n\n\nTools\n\n\n  awesome-scalability\n  developer-roadmap\n  hubcommander\n\n"
+    "excerpt": "\n    \n\n\nArticles\n\n\n  cloudbees / 8-cicd-best-practices-your-devops-journey\n  octo / gardez-les-cles-de-votre-infrastructure-a-labri-avec-vault\n\n\nTools\n\n\n  awesome-scalability\n  developer-roadmap\n  hubcommander\n\n"
 },{
     "title": "Chaos Monkey for Spring Boot",
     "link": "/dev/devops/chaos-monkey-spring-boot.html",
